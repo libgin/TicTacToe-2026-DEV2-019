b4bcdb8a5abd34f9e0d8e58671e1f92eef11a537
2026-DEV2-019 <2026-DEV2-019@users.noreply.github.com>
2026-01-19 20:40:19 +0100

    init project

---
+++ TicTacToe/TicTacToe/TicTacToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeApp.swift
+//  TicTacToe
+//
+//  Created by Victor on 1/19/26.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

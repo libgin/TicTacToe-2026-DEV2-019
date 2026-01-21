d64057f69db9f1735aea52651919374c6ef7a8f5
2026-DEV2-019 <2026-DEV2-019@users.noreply.github.com>
2026-01-21 13:04:31 +0100

    T view model updates after move

--- TicTacToe/TicTacToeTests/Presentation/GameViewModelTests.swift
+++ TicTacToe/TicTacToeTests/Presentation/GameViewModelTests.swift
@@ -18,4 +18,12 @@ final class GameViewModelTests: XCTestCase {
         XCTAssertTrue(viewModel.cells.allSatisfy { $0.count == 3 })
         XCTAssertTrue(viewModel.cells.flatMap { $0 }.allSatisfy { $0 == nil })
     }
+    
+    func test_playUpdatesBoardAndNextPlayer() throws {
+        let viewModel = GameViewModel()
+        try viewModel.play(row: 0, column: 0)
+        
+        XCTAssertEqual(viewModel.cells[0][0], .x)
+        XCTAssertEqual(viewModel.statusText, "Next: O")
+    }
 }
